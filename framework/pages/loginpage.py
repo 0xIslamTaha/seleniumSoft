e6f683768be9bf9ac9f1a0509363db39557c648e
islamTaha <tahai@codescalers.com>
2017-06-29 09:57:13 +0200

    add more login methods

--- framework/pages/loginpage.py
+++ framework/pages/loginpage.py
@@ -6,12 +6,23 @@ class LoginPage(BaseTest):
         self.get_page(page_url=self.url)
 
     def login_as_admin(self, username, password):
-        self.get_login_page()
-        self.set_text(element='username', value=username)
-        self.set_text(element='password', value=password)
-        self.find_element(element='password').submit()
+        self.login(username, password)
         try:
             self.find_element(element='admin_setting')
             return True
         except:
             return False
+
+    def login_as_user(self, username, password):
+        self.login(username, password)
+        try:
+            self.find_element(element='admin_setting')
+            return True
+        except:
+            return False
+
+    def login(self, username, password):
+        self.get_login_page()
+        self.set_text(element='username', value=username)
+        self.set_text(element='password', value=password)
+        self.find_element(element='password').submit()
