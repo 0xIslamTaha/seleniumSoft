5f03f6406fbb4474ac82f6118669f14bef6cefd2
islamTaha12 <tahai@codescalers.com>
2017-06-29 10:15:19 +0200

    add description

--- testcases/login/test_login_page.py
+++ testcases/login/test_login_page.py
@@ -4,20 +4,40 @@ import uuid
 
 class Login(LoginPage):
     def test01_login_as_admin(self):
+        """ ZST-001
+        *Test login as admin*
+        **Test Scenario:**
+        #. Login using correct credential, should succeed
+        """
         self.assertTrue(self.login_as_admin(username=self.admin_username,
                                             password=self.admin_password))
 
     def test02_login_with_wrong_username(self):
+        """ ZST-002
+        *Test login with wrong username*
+        **Test Scenario:**
+        #. Login using wrong username, should fail
+        """
         username = str(uuid.uuid4())
         self.assertFalse(self.login_as_admin(username=username,
                                              password=self.admin_password))
 
     def test03_login_with_wrong_password(self):
+        """ ZST-003
+        *Test login with wrong password*
+        **Test Scenario:**
+        #. Login using wrong password, should fail
+        """
         password = str(uuid.uuid4())
         self.assertFalse(self.login_as_admin(username=self.admin_username,
                                              password=password))
 
     def test04_login_with_wrong_username_wrong_password(self):
+        """ ZST-004
+        *Test login with wrong username and wrong password*
+        **Test Scenario:**
+        #. Login using wrong username and wrong password, should fail
+        """
         username = str(uuid.uuid4())
         password = str(uuid.uuid4())
         self.assertFalse(self.login_as_admin(username=username,
--- testcases/users/test_users_page.py
+++ testcases/users/test_users_page.py
@@ -5,6 +5,13 @@ from framework.pages.userspage import UsersPage
 class Users(UsersPage):
     @unittest.skip('https://zisoft.atlassian.net/browse/ZIS-233')
     def test01_create_new_user(self):
+        """ ZST-005
+        *Admin create new user*
+        **Test Scenario:**
+        #. Login as admin.
+        #. Create new user, should succeed.
+        #. Login as a new user, should succeed.
+        """
         self.login_page.login_as_admin(username=self.admin_username,
                                        password=self.admin_password)
         username, password = self.create_new_user(role='User')
