e4d8186a3aa5b73257154786078f964216085f57
islamTaha12 <tahai@codescalers.com>
2017-06-29 10:01:05 +0200

    create new user

--- testcases/users/test_users_page.py
+++ testcases/users/test_users_page.py
@@ -1,18 +1,12 @@
+import unittest
 from framework.pages.userspage import UsersPage
-from framework.pages.loginpage import LoginPage
-from framework.navigation.main_menu import MainMenu
 
 
 class Users(UsersPage):
-    def __init__(self):
-        super().__init__()
-        self.login_page = LoginPage()
-        self.main_menu = MainMenu()
-
+    @unittest.skip('https://zisoft.atlassian.net/browse/ZIS-233')
     def test01_create_new_user(self):
         self.login_page.login_as_admin(username=self.admin_username,
                                        password=self.admin_password)
-        self.assertTrue(self.main_menu.get_users_page(), "Can't get users page")
-        self.click('users_action_button')
-        self.click('click_manual')
+        username, password = self.create_new_user(role='User')
+        self.assertTrue(self.login_page.login_as_user(username, password))
 
